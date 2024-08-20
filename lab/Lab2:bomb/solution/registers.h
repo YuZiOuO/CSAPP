1d86072be84e5e37cf02381d2c6e79f9f5e8e727
kanata <cyz050312@gmail.com>
2024-08-20 15:37:19 +0800

    Lab2:bomb Phase2 defused; TODO:figure out how __isoc99_sscanf@plt and read_line works

--- lab/Lab2:bomb/solution/registers.h
+++ lab/Lab2:bomb/solution/registers.h
@@ -1,19 +1,24 @@
-int rdi,rsi,rdx,rcx,r8,r9;//arg
-int rax;//return val
-int rsp;//stack
-int r10,r11;//caller saved
-int rbx,rbp,r12,r13,r14,r15;//callee saved
-int arg1,arg2,arg3,arg4,arg5,arg6;
+int* rdi,rsi,rdx,rcx,r8,r9;//arg
+int* rax;//return val
+int* rsp;//stack
+int* r10,r11;//caller saved
+int* rbx,rbp,r12,r13,r14,r15;//callee saved
+//QWORD
 
-int al,bl,cl,dl,
+int* al,bl,cl,dl,
     sil,dil,bpl,spl,
     r8b,r9b,r10b,r11b,
     r12b,r13b,r14b,r15b;//BYTE
-int ax,bx,cx,dx,
+int* ax,bx,cx,dx,
     si,di,bp,sp,
     r8w,r9q,r10w,r11w,
     r12w,r13w,r14w,r15w;//WORD
-int eax,ebx,ecx,edx,
+int* eax,ebx,ecx,edx,
     esi,edi,ebp,esp,
     r8d,r9d,r10d,r11d,
     r12d,r13d,r14d,r15d;//DWORD
+
+int* arg1,arg2,arg3,arg4,arg5,arg6;
+void stackPush(int*);
+void stackPop(int*);
+void explode_bomb();
