c863882e5cd84d71bade0d6346513d0e8ff3fc58
kanata <cyz050312@gmail.com>
2024-08-24 14:48:08 +0800

    Lab2:bomb Phase5 defused

--- lab/Lab2:bomb/solution/phase5.c
+++ lab/Lab2:bomb/solution/phase5.c
@@ -14,7 +14,8 @@ int phase5(){
         goto L1;
     }
     goto L1;
-    L2:ecx = rbx+rax;
+    L2:ecx = *
+    (rbx+rax);
     *(rsp) = cl;
     rdx = *rsp;
     edx &= 0xf;
@@ -53,21 +54,26 @@ int phase5_readable(){
     rbx = rdi;
     ?rax = %fs:0x28;
     *(rsp+18) = rax;
-    if(string_length(??)!= 6){
+    if(string_length(rdi)!= 6){ //六位字符指针
         explode_bomb();
     }
     for(eax=0;rax!=6;rax++){
-        ecx = rbx+rax;
+        ecx = *(rbx+rax);
         *(rsp) = cl;
         rdx = *rsp;
         edx &= 0xf;
         edx = *(rdx+0x4024b0);
+        //0x4024b0: 6d616475 69657273 6e666f74 7662796c  maduiersnfotvbyl
+        //"flyers" = 66 6C 79 65 72 73
+        //edx: 0x9 0xf 0xe 0x5 0x6 0x7
+        //对照ASCII表找16进制数第二位为上述的字符
+        //ionuvw
         *(rsp+rax+16) = dl;
     }
-    *(rsp+16) = 0;//byte;
-    esi = 0x40245e;
-    rdi = rsp+10;
-    if(strings_not_equal(??);!=0){
+    *(rsp+0x16) = 0;//byte;
+    esi = 0x40245e;//"flyers"
+    rdi = rsp+16;
+    if(strings_not_equal(edi,esi)!=0){
         explode_bomb();
     }
     rax = *(rsp+0x18);
